f789b0fbae9a8df256592031164169eb11d96599
Elizabeth Alexandrova <elizawetaaleksandrowa@gmail.com>
2023-06-18 20:17:53 +0700

    some clean up

--- charts/mainwindow.cpp
+++ charts/mainwindow.cpp
@@ -8,30 +8,6 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-
-//    QHBoxLayout* hLayout = new QHBoxLayout(this);
-
-//    filesView = new FilesView(this);
-//    topButtons = new TopButtons(this);
-
-    //QVBoxLayout *hLayout = new QVBoxLayout(this);
-
-//    hLayout->addWidget(new TopButtons(this));
-//    hLayout->addWidget(topButtons);
-    //hLayout->addWidget(filesView);
-    //hLayout->addStretch();
-
-//    QSplitter *splitter = new QSplitter(this);
-//    tableView = new QTableView;
-//    tableView->setModel(rightPartModel);
-    //splitter->addWidget(filesView);
-    //splitter->addWidget(topButtons);
-    //setCentralWidget(splitter);
-
-//    hLayout->addWidget(filesView);
-//    hLayout->addWidget(topButtons);
-
 }
 
 MainWindow::~MainWindow()
